573cff2b955c860550e4754f0ba4b693757b68ed
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2021-12-21 19:17:22 +0530

    Add intro animation

--- views/index.js
+++ views/index.js
@@ -18,3 +18,11 @@ const app = ()=>{
     navSlide();
 
 }
+
+const tl = gsap.timeline({defaults: {ease: "power1.out"}})
+
+tl.to(".hide-text", {y: "0%", duration: 1, stagger: 0.25});
+tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
+tl.to(".intro", {y: "-100%", duration: 1} , "-=1");
+tl.fromTo(".nav" , {opacity: 0}, {opacity: 1 , duration: 1});
+tl.fromTo(".heading" , {opacity: 0}, {opacity: 1 , duration: 1}, "-=1");
