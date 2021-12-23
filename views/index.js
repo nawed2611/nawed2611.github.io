c588209c304ba0ecda321dc3c008e54e3e1db805
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2021-12-23 15:16:42 +0530

    Add langList cards

--- views/index.js
+++ views/index.js
@@ -1,28 +1,9 @@
-const navSlide = () => {
-    const svg = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } })
 
-    //Toggle Nav
-    svg.addEventListener('click', () => {
-    nav.classList.toggle('nav-active')
+tl.to(".hide-text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo(".nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".heading", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
-    //Animate Nav-links
-    navLinks.forEach((link, index) =>{
 
-    })
-});
-}
-
-const app = ()=>{
-    navSlide();
-
-}
-
-const tl = gsap.timeline({defaults: {ease: "power1.out"}})
-
-tl.to(".hide-text", {y: "0%", duration: 1, stagger: 0.25});
-tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5});
-tl.to(".intro", {y: "-100%", duration: 1} , "-=1");
-tl.fromTo(".nav" , {opacity: 0}, {opacity: 1 , duration: 1});
-tl.fromTo(".heading" , {opacity: 0}, {opacity: 1 , duration: 1}, "-=1");
