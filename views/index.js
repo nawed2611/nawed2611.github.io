89ae57b85341bdef75267d38943d4c6e4f02c332
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2021-12-26 18:00:51 +0530

    Add about-me animation

--- views/index.js
+++ views/index.js
@@ -5,3 +5,6 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo(".nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".heading", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+
+gsap.to('.about-me', {x: 170, duration: 5, stagger: 0.5, delay: 1})
