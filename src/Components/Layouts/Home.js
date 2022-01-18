c01214bb5fe2910afaa2d3cb5df1323eabd52ec5
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2022-01-19 01:12:55 +0530

    Add headerText animation

--- src/Components/Layouts/Home.js
+++ src/Components/Layouts/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../Styles/homeStyle.css';
 import Nawed from '../../Images/nawed.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+AOS.init();
 
 
 function Home() {
@@ -8,7 +12,14 @@ function Home() {
         <div className="headerDiv">
             <div className="headerText">
                 <h1>Hi, I'm NAWED ALI.</h1>
-                <h5>MERN Stack Developer Based in New Delhi, India</h5>    
+                <div className="animatep">
+                    <div className="titleWrapper">
+                        <div className="titleId">MERN Stack Developer</div>
+                        <div className="titleId">Content Writer</div>
+                        <div className="titleId">Open Source Developer</div>
+                    </div>
+                </div>
+                <h5>Based in New Delhi, India</h5>
             </div>
             <div className="profDiv">
                     <img src={Nawed} />
