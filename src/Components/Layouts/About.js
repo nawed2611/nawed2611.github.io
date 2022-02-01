0d91543d74f3ca0e43373de93b35fcb54fe266c8
Nawed Ali <83456083+nawed2611@users.noreply.github.com>
2022-02-01 15:09:30 +0530

    Change font and UI

--- src/Components/Layouts/About.js
+++ src/Components/Layouts/About.js
@@ -1,62 +1,71 @@
 import React from 'react';
 import '../Styles/aboutStyle.css';
 
+import AboutImage from '../../Images/menitrust.png';
+
 function About() {
     return (
         <div className="about">
             <div data-aos="fade-up" className="aboutDiv">
                 <div data-aos="fade-up" data-aos-delay="200" className="aboutText">
-                    <h1 className="helloText">HELLO THERE!</h1>
-                        <div className="aboutList">
-                            <h2>My name is Nawed and I'm a MERN Stack Creative developer with keen interest in WEBGL animations.</h2>
+                    <h1 className="helloText">Hello There!</h1>
+                    <div className="aboutList">
 
-                            <h2>I'm a BTech Computer Science Engineering Student, Batch of 2024.</h2>
+                        <div>
+                            <p>My name is Nawed and I'm a MERN Stack Creative developer with keen interest in WEBGL animations. I'm a BTech Computer Science Engineering Student, Batch of 2024.</p>
 
-                            <h2>I love building beautiful and creative websites through JavaScript technologies.</h2>
-
-                            <h2>I'm an open-source enthusiast with history of working with different open-source communitites.</h2>
-
-                            <h2>Recently I've been planning on getting started with Machine Learning.</h2>  
                         </div>
+
+                        <div>
+
+
+                            <p>
+                                I love building beautiful and creative websites through JavaScript technologies.
+                                I'm an open-source enthusiast with history of working with different open-source communitites.
+                                Recently I've been planning on getting started with Machine Learning.</p>
+                        </div>
+
+
+                    </div>
                 </div>
-        
+
             </div>
 
             <div data-aos="fade-up" className="langDiv">
-                <h1 data-aos="fade-up" className="langText">Languages & Databases</h1>
-                <ul data-aos="fade-up" className="langList">
+                <h1 className="langText">Languages & Databases I'm fluent in</h1>
+                <ul className="langList">
                     <li>C</li>
                     <li>C++</li>
                     <li>HTML5</li>
                     <li>Python</li>
                     <li>JavaScript</li>
-                    <li>PHP</li>                    
-                    <li>MongoDB</li>                    
+                    <li>PHP</li>
+                    <li>MongoDB</li>
                 </ul>
 
-                <h1 data-aos="fade-up" className="langText">Libraries & Frameworks</h1>
-                <ul data-aos="fade-up" className="langList">
+                <h1 className="langText">Libraries & Frameworks I've worked with</h1>
+                <ul className="langList">
                     <li>Node.js</li>
                     <li>Express.js</li>
                     <li>React.js</li>
                     <li>Three.js</li>
                     <li>MongoDB</li>
-                    <li>GSAP</li>   
+                    <li>GSAP</li>
                     <li>AOS</li>
                 </ul>
 
-                <h1 data-aos="fade-up"className="langText">Tools</h1>
-                <ul data-aos="fade-up"className="langList">
+                <h1 className="langText">Tools I know</h1>
+                <ul className="langList">
                     <li>Adobe Photoshop</li>
                     <li>Adobe Premiere Pro</li>
                     <li>Figma</li>
                     <li>Git</li>
-                    <li>Anima</li>                  
+                    <li>Anima</li>
                     <li>Microsoft Visual Studio Code</li>
                 </ul>
 
             </div>
-            
+
         </div>
     );
 }
--- src/Components/Layouts/Projects.js
+++ src/Components/Layouts/Projects.js
@@ -3,7 +3,7 @@ import '../Styles/projStyle.css';
 
 //Importing AOS Library
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 AOS.init();
 
 function Projects() {
@@ -11,6 +11,13 @@ function Projects() {
         <div className="projects">
             <div data-aos="fade-up" className="projDiv">
                 <h1 data-aos="fade-up" data-aos-delay="200">Experiences & Projects</h1>
+                <div className="projList">
+                    <div>
+                        <h3>Weather Web-App</h3>
+                        <p>lorem ipsum dolor sit amet, consectetur adip</p>
+                        <img src="" />
+                    </div>
+                </div>
             </div>
         </div>
     );
